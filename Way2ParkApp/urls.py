5c482206c28ddfb49e5a034e093480c322091d3a
andre <andreavalentini2025@gmail.com>
2023-02-01 17:23:35 +0100

    refactor del progetto

--- Way2ParkApp/urls.py
+++ Way2ParkApp/urls.py
@@ -1,4 +1,4 @@
-"""quickstartproject URL Configuration
+"""Way2ParkApp URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('hello_azure.urls')),
+    path('', include('Way2Park.urls')),
     path('admin/', admin.site.urls),
 ]
