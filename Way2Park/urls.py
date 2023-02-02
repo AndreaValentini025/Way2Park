58ace8edde42e3bbed86c70a26e5f15e8facfe26
andre <andreavalentini2025@gmail.com>
2023-02-02 19:38:39 +0100

    aggiunta funzione associazione pagamenti

--- Way2Park/urls.py
+++ Way2Park/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('hello', views.hello, name='hello'),
     path('homepage', views.ParcheggiListView.as_view(), name='homepage'),
+    path('link',views.formAssociaTargaMP, name='link'),
+    path('associazione', views.associazione, name='associazione'),
 ]
